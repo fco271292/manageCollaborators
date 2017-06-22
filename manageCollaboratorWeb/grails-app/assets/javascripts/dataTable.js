c3c8899d8f888ac3c9a190376cf2bd1340ccda9b
Francisco Reyes <fco271292@gmail.com>
2017-06-22 13:28:43 -0500

    #3 Se agrega dataTable JQuery

---
+++ manageCollaboratorWeb/grails-app/assets/javascripts/dataTable.js
@@ -0,0 +1,41 @@
+$.noConflict()
+jQuery(document).ready(function($) {
+    var table = $('#table').DataTable({
+        
+        "paging": true,
+        "lengthMenu": [[5, 10, 50, -1], [5, 10, 50, "Todos"]],
+        "language":{
+            "lengthMenu": "Mostrar _MENU_ ",
+            "info": "Pagina _PAGE_ - _PAGES_",
+            "search": "Buscar",
+            "paginate": {
+                "first":      "Primero",
+                "last":       "Ultimo",
+                "next":       ">>",
+                "previous":   "<<"
+            },
+            "infoFiltered":   "(Total de registros _MAX_ )",
+            "zeroRecords":    "No hay registros",
+            "infoEmpty":      "No hay registros"
+        },
+        "pagingType": "full_numbers",
+        "info": false,
+        "searching": true,
+        "lengthChange": false,
+        "dom":  "<'row'<'col-lg-12'tr>>" +
+                "<'row'<'col-lg-4'i><'col-lg-8'p>>"
+                /*  Iniciales para mostrar elementos del plugin
+                    t: Table
+                    r: Processing display element
+                    i: Table information summary
+                    p: Pagination control
+                */
+        
+    });
+
+    //Permite buscar desde una entrada alterna
+    $('#searchText').on( 'keyup', function () {
+        table.search(this.value).draw();
+    } );
+    
+} );
